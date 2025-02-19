823069f9471d86992d4cdfe11085ef9f491098ac
Tram46 <tramqn46@gmail.com>
2025-02-19 16:07:45 -0500

    BankAccount Class framework

---
+++ BankAccount.java
@@ -0,0 +1,20 @@
+public class BankAccount {
+    private String accountNumber;
+    private double balance;
+
+    public BankAccount(String accountNumber, double initialBalance) {
+
+    }
+
+    public void deposit(double amount) {
+
+    }
+
+    public void withdraw(double amount) {
+
+    }
+
+    public void checkBalance() {
+
+    }
+}
